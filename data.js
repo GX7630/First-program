a0abb0ef57feb32a2e47aabde00d323b7a86c3d8
GX7630 <3310888934@qq.com>
2025-10-27 04:30:54 +0800

    Add data.js for application configuration and info
    This file contains configuration, announcements, changelog, and software information data for the application.

---
+++ data.js
@@ -0,0 +1,47 @@
+// 数据文件 - 所有数据存储在这里
+// 其他文件从该文件读取数据
+
+// 系统配置数据
+let systemConfig = {
+    "pageTitle": "The program 1",
+    "navTitle": "Rayix",
+    "heroTitle": "Archives Query System",
+    "heroSubtitle": "The first program of Rayix",
+    "footerText": "© 2026 Rayix 版权所有",
+    "downloadFilename": "ArchiveQuerySystem.exe"
+};
+
+// 公告数据
+let announcements = [
+    {
+        "id": 2,
+        "title": "About download",
+        "content": "The download link will be provide soon",
+        "date": "2025-10-26"
+    },
+    {
+        "id": 1,
+        "title": "About Language",
+        "content": "Language changed to English in Oct.26",
+        "date": "2025-10-26"
+    }
+];
+
+// 更新日志数据
+let changelog = [
+    {
+        "id": 1,
+        "version": "V1.1.1",
+        "date": "2025-10-26",
+        "content": "Refine the input of the archives \nRepair the mistake when input in research archives "
+    }
+];
+
+// 软件信息数据
+let softwareInfo = {
+    "version": "V1.1.0",
+    "fileSize": "3KB",
+    "updateDate": "2025-10-27",
+    "downloadUrl": "https://example.com/ArchiveQuerySystem.exe",
+    "description": "档案查询系统是一款专为档案管理设计的应用程序，支持快速查询、检索和管理各类档案信息。"
+};
